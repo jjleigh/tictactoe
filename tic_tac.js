e4ceb8b516969ba246f266a388a6f235b39c2b61
justine jones <justinejones@justines-MacBook-Air.local>
2014-07-07 14:48:07 -0400

    got the box links working

--- tic_tac.js
+++ tic_tac.js
@@ -1,7 +1,23 @@
-function player1() {
+$(function() {
 
-}
+	var turn = 1
 
-function player2() {
+	// when someone clicks any of the canvas boxes, i want to know what turn it is and then increment the turn
+
+	$('td a').on('click',function() {
+
+		if (turn % 2 == 0 ) {
+			$(this).text("X")
+		} else {
+			$(this).text("O")
+		}
+		turn++
+	});
+
+	// 
+
+});
 	
-}
+
+// winningCombination = [[0,1,2],[3,4,5],[6,7,8],[0,3,6],[1,4,7],[2,5,8],[0,4,8],[2,4,6]];
+

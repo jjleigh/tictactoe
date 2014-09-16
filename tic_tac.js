90d3e279e065004f0531e6423fcda79038d32a3d
Justine Jones <justinejones91@gmail.com>
2014-09-15 21:15:42 -0400

    removed duplicate prompt

--- tic_tac.js
+++ tic_tac.js
@@ -22,12 +22,12 @@ $(document).ready(function() {
 			turn++
 			if (turn >= 5) { 
 				checkForWinner();
-						if (turn === 10) {
-							var answer = prompt("no one wins, would you like to play again");
-							if (answer == "yes") {
-								location.reload();
-							}		
-						}
+						// if (turn === 10) {
+						// 	var answer = prompt("no one wins, would you like to play again");
+						// 	if (answer == "yes") {
+						// 		location.reload();
+						// 	}		
+						// }
 			}
 		});
 	};
